962dc02694ba0d3e41ff5b1f558d4899cc226e7a
isshin0708 <isshin07081240@icloud.com>
2025-12-10 10:55:45 +0900

    2025 1210

--- src/main/java/com/example/JavaFurim/repository/UserComplaintRepository.java
+++ src/main/java/com/example/JavaFurim/repository/UserComplaintRepository.java
@@ -1,5 +1,14 @@
 package com.example.JavaFurim.repository;
 
-public interface UserComplaintRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.JavaFurim.entity.UserComplaint;
+
+public interface UserComplaintRepository extends JpaRepository<UserComplaint, Long> {
+
+	long countByReportedUserId(Long reportedUserId);
+
+	List<UserComplaint> findByReportedUserIdOrderByCreatedAtDesc(Long reportedUserId);
 }
